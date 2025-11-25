ee0046d9e31c3ef1380aa75a64d6a990c94a771a
iharshlalakiya <harshlalakiya.me@gmail.com>
2025-11-25 20:04:17 +0530

    code update

--- main.py
+++ main.py
@@ -1,112 +1,66 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+import textwrap
 
 app = FastAPI()
 
 @app.get("/unit1")
 def unit1():
-    r_code = """
-    is_prime <- function(n) {{
-    if (n < 2) return(FALSE)
-    if (n == 2) return(TRUE)
-    if (n %% 2 == 0) return(FALSE)
-    for (i in seq(3, floor(sqrt(n)), by=2)) {{
-        if (n %% i == 0) return(FALSE)
-    }}
-    TRUE
-    }}
+    return textwrap.dedent("""
+    factorial_func <- function(n) {
+      if (n == 0) return(1)
+      prod(1:n)
+    }
 
-    primes_up_to <- function(limit) {{
-    which(sapply(1:limit, is_prime))
-    }}
-
-    primes_up_to({limit})
-    """
-    return {"r_code": r_code}
+    factorial_func(10)
+    """).strip()
 
 @app.get("/unit2")
 def unit2():
-    r_code = """
-    is_prime <- function(n) {{
-    if (n < 2) return(FALSE)
-    if (n == 2) return(TRUE)
-    if (n %% 2 == 0) return(FALSE)
-    for (i in seq(3, floor(sqrt(n)), by=2)) {{
-        if (n %% i == 0) return(FALSE)
-    }}
-    TRUE
-    }}
+    return textwrap.dedent("""
+    factorial_func <- function(n) {
+      if (n == 0) return(1)
+      prod(1:n)
+    }
 
-    primes_up_to <- function(limit) {{
-    which(sapply(1:limit, is_prime))
-    }}
-
-    primes_up_to({limit})
-    """
-    return {"r_code": r_code}
+    factorial_func(10)
+    """).strip()
 
 @app.get("/unit3")
 def unit3():
-    r_code = """
-    is_prime <- function(n) {{
-    if (n < 2) return(FALSE)
-    if (n == 2) return(TRUE)
-    if (n %% 2 == 0) return(FALSE)
-    for (i in seq(3, floor(sqrt(n)), by=2)) {{
-        if (n %% i == 0) return(FALSE)
-    }}
-    TRUE
-    }}
+    return textwrap.dedent("""
+    factorial_func <- function(n) {
+      if (n == 0) return(1)
+      prod(1:n)
+    }
 
-    primes_up_to <- function(limit) {{
-    which(sapply(1:limit, is_prime))
-    }}
-
-    primes_up_to({limit})
-    """
-    return {"r_code": r_code}
+    factorial_func(10)
+    """).strip()
 
 @app.get("/unit4")
 def unit4():
-    r_code = """
-    is_prime <- function(n) {{
-    if (n < 2) return(FALSE)
-    if (n == 2) return(TRUE)
-    if (n %% 2 == 0) return(FALSE)
-    for (i in seq(3, floor(sqrt(n)), by=2)) {{
-        if (n %% i == 0) return(FALSE)
-    }}
-    TRUE
-    }}
+    return textwrap.dedent("""
+    factorial_func <- function(n) {
+      if (n == 0) return(1)
+      prod(1:n)
+    }
 
-    primes_up_to <- function(limit) {{
-    which(sapply(1:limit, is_prime))
-    }}
-
-    primes_up_to({limit})
-    """
-    return {"r_code": r_code}
+    factorial_func(10)
+    """).strip()
 
 @app.get("/unit5")
 def unit5():
-    r_code = """
-    is_prime <- function(n) {{
-    if (n < 2) return(FALSE)
-    if (n == 2) return(TRUE)
-    if (n %% 2 == 0) return(FALSE)
-    for (i in seq(3, floor(sqrt(n)), by=2)) {{
-        if (n %% i == 0) return(FALSE)
-    }}
-    TRUE
-    }}
+    return textwrap.dedent("""
+    factorial_func <- function(n) {
+      if (n == 0) return(1)
+      prod(1:n)
+    }
 
-    primes_up_to <- function(limit) {{
-    which(sapply(1:limit, is_prime))
-    }}
-
-    primes_up_to({limit})
-    """
-    return {"r_code": r_code}
+    factorial_func(10)
+    """).strip()
 
 @app.get("/")
 def home():
-    return {"message": "tari masi no piko"}
+    return textwrap.dedent("""
+    tari masi no piko
+    """).strip()
